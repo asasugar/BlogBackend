7b14e1ffa9e6f8d79e464dd60dcfc6be4de34722
xxj95719 <87416959@qq.com>
2018-10-24 14:39:16 +0800

    数据库文件

---
+++ db/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` VALUES (1, '我是你鸡哥', 'root', 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png', 'Just do it', 'root');
+INSERT INTO `user` VALUES (10, '天呐我的博客', 'qwe', 'https://thirdwx.qlogo.cn/mmopen/vi_32/PrUDxP61MBnVAMWsKUPLYmreuPEAaClGmUfrnvI7XYAZtXiaM3GnNWrbVXMe5kum8ptYrDbWsVHO2u8vdFDDgag/132', '你个龟龟', 'aqwe');
+INSERT INTO `user` VALUES (12, '超级管理员', '123456', 'http://pcmhhpvjl.bkt.clouddn.com/1539249433915', '很皮', 'super');
